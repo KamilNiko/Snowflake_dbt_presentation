a597c0e4a7f29b163c0846dcaac62188d17383e5
KamilNiko <kamil.kowalczyk@housecallpro.com>
2025-11-25 16:26:50 +0000

    2 models changed

--- Snowflake_dbt_POC/models/my_first_dbt_model.sql
+++ Snowflake_dbt_POC/models/my_first_dbt_model.sql
@@ -1,9 +1,13 @@
 {{ config(materialized='table') }}
 
-  with source_data as (
-      select 1 as id
-      union all
-      select null as id
+    with source_data as (
+    select 1 as id
+    union all
+    select null as id -- Zostawiamy wiersz z NULL
+    union all
+    select 3 as id   
+    union all
+    select 4 as id    
   )
 
   select *
